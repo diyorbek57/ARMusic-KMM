ecff7fd17871c25734b9f0969ac7baebe8a34bad
diyorbek57 <Diyorbek1727>
2022-10-11 05:52:18 +0500

    Initial commit

---
+++ shared/src/commonMain/kotlin/uz/ayizor/armusic/Greeting.kt
@@ -0,0 +1,9 @@
+package uz.ayizor.armusic
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greeting(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
